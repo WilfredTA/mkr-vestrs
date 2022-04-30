9b83c0c15c28bb719d39097d38a931e7d39bc500
Tannr <tannr@hey.com>
2022-04-30 19:22:34 -0400

    add ds-token dep, basic check calls

--- dss-vest-rs/src/lib.rs
+++ dss-vest-rs/src/lib.rs
@@ -1,5 +1,6 @@
 mod bindings {
     include!(concat!(env!("OUT_DIR"), "/dss-vest.rs"));
+    //include!(concat!(env!("OUT_DIR"), "/ds-token.rs"));
 }
 
 pub use bindings::*;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use dss_vest_rs::{DssVestTransferrable, DSSVESTTRANSFERRABLE_ABI};
 use ethers::prelude::*;
+use ethers::abi::AbiDecode;
 mod opts;
-use opts::{Opts, Subcommands};
+use opts::{Opts, Subcommands, ClaimOpts, CheckOpts, InnerOpts};
 use structopt::StructOpt;
-
+use anyhow::Result;
 #[tokio::main]
 async fn main() {
     let opts = Opts::from_args();
@@ -13,10 +14,30 @@ async fn main() {
 
         },
         Subcommands::Check(check_opts) => {
-
+            check(check_opts).await;
         },
         Subcommands::Mkr(mkr_cmd) => {
             
         },
     }
+}
+
+async fn claim_token(opts: ClaimOpts) {
+    
+}
+
+async fn check(opts: CheckOpts) -> Result<()>{
+    let CheckOpts { id, inner, usr } = opts;
+    
+    let InnerOpts {contract, eth } = inner;
+
+    let provider = eth.get_client()?;
+    let contract = DssVestTransferrable::new(contract.addr, provider.clone());
+    if let Some(id) = id {
+        let award = contract.awards(id).call().await?;
+        println!("Award for id {:#?} is {:#?}", id, award);
+    }
+
+   
+    Ok(())
 }
